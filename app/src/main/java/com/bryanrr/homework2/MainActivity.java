bd01e825410df96e33e5a574a6082d51c9ba481b
bryan <bryan.rr@hotmail.com>
2020-02-21 21:33:15 -0500

    go

--- app/src/main/java/com/bryanrr/homework2/MainActivity.java
+++ app/src/main/java/com/bryanrr/homework2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.bryanrr.homework2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.tb_main);
+        setSupportActionBar(toolbar);
     }
 }
